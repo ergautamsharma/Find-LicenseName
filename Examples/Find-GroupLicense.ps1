6df94ddf748593b0eccb3ec466ac7d65f85dd981
Gautam Sharma <33420080+ergautamsharma@users.noreply.github.com>
2021-07-24 13:50:22 +0530

    Update Find-GroupLicense.ps1

--- Examples/Find-GroupLicense.ps1
+++ Examples/Find-GroupLicense.ps1
@@ -1,4 +1,6 @@
 #Connect to MSOLService (Connect-MsolService):
 #Store MSOL group proprities to a variable as in below example:
 $Group = Get-MsolGroup -ObjectId xxxx5489-xx80-xxxx-80b8-8bxxf280xxxx 
+
+#To find the which all license assign to this Group use below PS:
 Find-LicenseName -MSOLGroup $Group
